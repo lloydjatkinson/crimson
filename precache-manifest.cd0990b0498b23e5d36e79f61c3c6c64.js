71741750701b7bcd5c7cf36234c6053fb0c1b334
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-08 15:55:03 +0000

    Deploy lloydjatkinson/crimson to github.com/lloydjatkinson/crimson.git:gh-pages

---
+++ precache-manifest.cd0990b0498b23e5d36e79f61c3c6c64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5e6447c10781637e527",
+    "url": "/js/vendor.a5e6447c.js"
+  },
+  {
+    "revision": "ce28c628eea246b643c9",
+    "url": "/js/manifest.ce28c628.js"
+  },
+  {
+    "revision": "ae3876b7ea79a784bb13",
+    "url": "/js/app.ae3876b7.js"
+  },
+  {
+    "revision": "d8b8ac171c868a12e8c42a4597e46525",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae3876b7ea79a784bb13",
+    "url": "/css/app.1d0f45e7.css"
+  }
+];
